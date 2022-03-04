00772c7f356bcf7fc91716220fe8264a48b3f08a
IngridRochap <rmpgui@gmail.com>
2022-03-04 17:22:10 -0300

    cadastro

---
+++ cadastro.js
@@ -0,0 +1,5 @@
+let listaParticipantes = ["Maria"];
+let quantiParticipantes = listaParticipantes.length;
+prompt('Digite um nome');
+var primeiro = prompt('Digite um nome');
+console.log(primeiro);
